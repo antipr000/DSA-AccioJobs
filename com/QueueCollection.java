80aace1009fcda96f40e3006884e477419077788
Soham Mukherjee <rockstarsam1234@gmail.com>
2021-08-04 22:06:29 +0530

    Sliding window min

--- com/QueueCollection.java
+++ com/QueueCollection.java
@@ -1,8 +1,58 @@
-import java.util.*;
+package com;
+
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Deque;
 
 public class QueueCollection{
     public static void main() {
-        Queue<Integer> q = new LinkedList<Integer>();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        while (!q.isEmpty()){
+            System.out.println(q.poll());
+        }
+
+        Deque<Integer> dq = new LinkedList<>();
+        dq.offerFirst(2); //add at front
+        dq.offerFirst(3);
+        dq.offerLast(4);
+        System.out.println(dq);
+
+        System.out.println(dq.pollFirst());
+
+        System.out.println(dq.pollLast());
+
+    }
+}
+
+class SlidingWindowMin{
+    public void slidingWindowMinimum(int []a, int k){
+        Deque<Integer> d = new LinkedList<>();
+        int n = a.length;
+        int [] ans = new int[n-k+1];
+
+        for(int i=0; i<k; i++){
+            while(!d.isEmpty() && d.peekLast() > a[i]){
+                d.pollLast();
+            }
+            d.offerLast(a[i]);
+        }
+
+        ans[0] = d.peekFirst();
+        for(int i=k; i<n; i++){
+            if(a[i-k] == d.peekFirst()){
+                d.pollFirst();
+            }
+
+            while(!d.isEmpty() && d.peekLast() > a[i]){
+                d.pollLast();
+            }
+
+            d.offerLast(a[i]);
+            ans[i-k+1] = d.peekFirst();
+        }
     }
 }
 
@@ -29,7 +79,7 @@ class QueueImpl{
         return false;
     }
 
-    void push(int val){
+    void add(int val){
         //Add an element at the back of the queue
 
         if(head == null){
